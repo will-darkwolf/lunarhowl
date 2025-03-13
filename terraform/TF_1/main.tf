b28e448120ad546782db068910258a837e5c5eba
Will Kline <will.kline@darkwolfsolutions.com>
2025-03-13 17:03:02 -0400

    terraform fmt

--- terraform/TF_1/main.tf
+++ terraform/TF_1/main.tf
@@ -11,7 +11,7 @@ variable "bucket_name" {
   type        = string
   description = "The name of the GCS bucket"
   validation {
-    condition = can(regex("^[a-z]{1,5}$", var.bucket_name))
+    condition     = can(regex("^[a-z]{1,5}$", var.bucket_name))
     error_message = "Bucket name must contain no more than 5 letters, all lower-case."
   }
 }
@@ -26,7 +26,7 @@ resource "random_string" "suffix" {
 # Create a GCS bucket
 resource "google_storage_bucket" "wolf-bucket" {
   name     = "wolf-${var.bucket_name}-${random_string.suffix.result}" # Choose a unique bucket name
-  location = "US" # Specify the desired region for your bucket
+  location = "US"                                                     # Specify the desired region for your bucket
 
   # Optional: Enable uniform bucket level access
   # This allows granular control over object permissions
--- terraform/TF_2/main.tf
+++ terraform/TF_2/main.tf
@@ -12,7 +12,7 @@ variable "bucket_name" {
   type        = string
   description = "The name of the GCS bucket"
   validation {
-    condition = can(regex("^[a-z]{1,5}$", var.bucket_name))
+    condition     = can(regex("^[a-z]{1,5}$", var.bucket_name))
     error_message = "Bucket name must contain no more than 5 letters, all lower-case."
   }
 }
@@ -20,15 +20,15 @@ variable "bucket_name" {
 resource "random_string" "suffix" {
   length  = 4
   special = false
-  lower = true
-  upper = false
+  lower   = true
+  upper   = false
 }
 
 
 # Create a GCS bucket
 resource "google_storage_bucket" "wolf-bucket" {
-  name = "Wolf-${var.bucket_name}-${random_string.suffix.result}" # Choose a unique bucket name
-  location = "US" # Specify the desired region for your bucket
+  name     = "Wolf-${var.bucket_name}-${random_string.suffix.result}" # Choose a unique bucket name
+  location = "US"                                                     # Specify the desired region for your bucket
 
   # Optional: Enable uniform bucket level access
   # This allows granular control over object permissions
